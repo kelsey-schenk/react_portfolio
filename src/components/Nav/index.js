26901bc06106127d3f7c947e7ef729b76dceacce
kelsey-schenk <kelsey.schenk@att.net>
2022-03-26 15:14:07 -0400

    started nav index

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React, {  useState } from 'react';
+import React, { useEffect } from 'react';
+
+
+function Nav(props) {
+    return (
+        <header class>
+            <h2>
+                <a>Kelsey Schenk's Portfolio</a>
+            </h2>
+        </header>
+    )
+}
