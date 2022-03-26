b800041988915cc27a6008862cf4ac3397260b7c
kelsey-schenk <kelsey.schenk@att.net>
2022-03-26 15:10:58 -0400

    app js edits

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Nav from './components/Nav';
+
+function App() {
+    const [projects] = useState([
+
+    ]);
+
+    return (
+        <div>
+            <Nav>
+
+            </Nav>
+            <main>
+                <Header></Header>
+                <Project></Project>
+                <Footer></Footer>
+            </main>
+        </div>
+    )
+}
